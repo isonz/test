93a5395ef15a3cbd3acf548738abe4d5e47fd082
Ison <ison.chang@gmail.com>
2016-08-02 15:40:18 +0800

    asdfsdf

--- site/webroot/export_DB/cron_db.sh
+++ site/webroot/export_DB/cron_db.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 bak_dir=/www/Bak/website/en.sanyatour.com/data;
-url=urls/en.sanyatour.com.url
+url=/www/Bak/website/en.sanyatour.com/urls/en.sanyatour.com.url
 date_now=`date +%Y`-`date +%m`-`date +%d`;
 filename=en.sanyatour.com.sql
 
 php export_db_get.php > $url
-wget -O $bak_dir/$filename -i $url
+wget -q -b -O $bak_dir/$filename -c -i $url
 
 tar zcvfP $bak_dir/$date_now.tar.gz $bak_dir/*.sql
 find $bak_dir -mtime +30 -name "*.*" -exec rm -rf {} \;
